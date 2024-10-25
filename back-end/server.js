e46639a4f26b89cadf5dcea19ac1cfb501e83875
Энх-Амар Болдбаатар <LPC240855@G401-CC0159.local>
2024-10-25 12:49:00 +0800

    aa

--- back-end/server.js
+++ back-end/server.js
@@ -17,7 +17,52 @@ app.post("/sign-up", async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const existingUser = await sql`SELECT * FROM users WHERE = ${email}`;
+    const existingUser =
+      await sql`SELECT * FROM "users" WHERE email = ${email}`;
+    if (existingUser.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = await sql`
+        INSERT INTO users (name, email, password) 
+        VALUES (${name}, ${email}, ${password})
+        RETURNING id, email`;
+
+    res
+      .status(201)
+      .json({ message: "user created successfully", user: newUser[0] });
+  } catch (error) {
+    res.status(201).json({ message: "Internal server error creating user" });
+  }
+});
+
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await sql`SELECT * FROM "users" WHERE email = ${email}`;
+    if (user.length === 0) {
+      return res.status(400).json({ message: "email or password not match" });
+    }
+
+    if (user[0].password !== password) {
+      return res.status(400).json({ message: "password not match" });
+    }
+
+    res.status(200).json({ message: "Login successful", user: user[0] });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error during login user" });
+  }
+});
+
+app.post("/records", async (req, res) => {
+  const { name, description ,category_icon } = req.body;
+
+  try {
+    const existingUser =
+      await sql`SELECT * FROM "users" WHERE email = ${email}`;
     if (existingUser.length > 0) {
       return res.status(400).json({ message: "User already exists" });
     }
